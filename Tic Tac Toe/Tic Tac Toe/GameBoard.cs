ffdf8bc76df61e7f585e3dd464da210b4bc4d899
Arrcenio <71286201+Arrcenio@users.noreply.github.com>
2020-09-15 13:34:30 +0200

    Add files via upload

---
+++ Tic Tac Toe/Tic Tac Toe/GameBoard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe
+{
+    class GameBoard
+    {
+        public int[,] spaces;
+        public int boardSize;
+
+        public GameBoard(int aSpaces)
+        {
+            spaces = IntializeBoard(aSpaces);
+            boardSize = aSpaces;
+
+            for (int i1 = 0; i1 < aSpaces; i1++)
+            {
+                for (int i2 = 0; i2 < aSpaces; i2++)
+                {
+                    spaces[i1, i2] = 0;
+                }
+            }
+        }
+
+        private static int[,] IntializeBoard(int size)
+        {
+            int[,] array = new int[size, size];
+            return array;
+        }
+    }
+}
